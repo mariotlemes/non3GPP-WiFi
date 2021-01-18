821ee59288247e30b51ed0c0c3d0b71a9aaa91ea
root <mario.lemes@ifg.edu.br>
2021-01-18 06:46:06 -0300

    Update utils/fix_core.sh file

--- utils/fix_core.sh
+++ utils/fix_core.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# enable forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# stoppig ufw service
 sudo /etc/init.d/ufw stop
 
+# compiling gtp5g module
 cd ~/gtp5g
 sudo make && sudo make install
 
+# adding rule in iptables
 sudo iptables -t nat -F
 sudo iptables -t nat -X
-
 sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
